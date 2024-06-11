97712ecf6ccb6eeaf1a83ed90f800b752e328b2a
Tim Van Eerdewegh <tim.vaneerdewegh@uantwerpen.be>
2024-06-11 12:10:09 +0200

    Use lazyvim instead of kickstart

---
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,17 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-h>", "<C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
