1f04b65c75ed1e1456c12f901c84c9c5c96e9ec9
tim.vaneerdewegh <tim.vaneerdewegh@klarrio.com>
2025-06-06 22:33:50 +0200

    chore: some changes made on desktop

---
+++ private_dot_config/zsh/aliases.sh
@@ -0,0 +1,3 @@
+alias vim=nvim
+alias tmuxconf="nvim ~/.config/tmux"
+alias zshconf="nvim ~/.config/zsh"
---
+++ private_dot_config/zsh/envvars.sh
@@ -0,0 +1,6 @@
+export ZSH_THEME="robbyrussell"
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+export GOPRIVATE=github.com/Klarrio/*
+export EDITOR=nvim
