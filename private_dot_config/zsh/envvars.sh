5097a2478726b369c407ea43ef4b5786f1509a85
tim.vaneerdewegh <tim.vaneerdewegh@klarrio.com>
2025-06-10 10:24:28 +0200

    Update .config/zsh/envvars.sh

--- private_dot_config/zsh/envvars.sh
+++ private_dot_config/zsh/envvars.sh
@@ -1,6 +1,6 @@
 export ZSH_THEME="robbyrussell"
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.krew/bin:$PATH
 export GOPRIVATE=github.com/Klarrio/*
 export EDITOR=nvim
