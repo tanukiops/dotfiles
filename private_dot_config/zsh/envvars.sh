ab470a94078e764bfefef8b18c4cc8b7c554a482
tim.vaneerdewegh <tim.vaneerdewegh@klarrio.com>
2025-12-04 11:34:11 +0100

    Update .config/zsh/envvars.sh

--- private_dot_config/zsh/envvars.sh
+++ private_dot_config/zsh/envvars.sh
@@ -2,6 +2,6 @@ export ZSH_THEME="robbyrussell"
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.krew/bin:$PATH
-export GOPRIVATE=github.com/Klarrio/*
+export GOPRIVATE=github.com/klarrio-kpn-dsh/*
 export EDITOR=nvim
 export ANSIBLE_VAULT_PASSWORD_FILE=~/.ansible/vault-password
