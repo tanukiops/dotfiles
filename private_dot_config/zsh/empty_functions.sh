6662fcce691fb41c3caa8c808efc2d68ad8196e1
tim.vaneerdewegh <tim.vaneerdewegh@klarrio.com>
2025-12-04 08:39:27 +0100

    Update .config/zsh/functions.sh

--- private_dot_config/zsh/empty_functions.sh
+++ private_dot_config/zsh/empty_functions.sh
@@ -1,8 +1,11 @@
 function tunnel() {
-  /home/tim/workspace/tools-devutil/dist/devutil ktunnel
+  /home/tim/workspace/tools-devutil/dist/devutil ktunnel --name "k8s-dev-aws-lz-dsh"
 }
 function kadmin() {
   /home/tim/workspace/tools-devutil/dist/devutil kupdate --dev --name $(kubectl config current-context)
+}
+function kconnect() {
+  kubie ctx $(fzf_clusters)
 }
 
 function dauth() {
@@ -11,4 +14,17 @@ function dauth() {
   export DAUTH_PASSWORD=$(bw get password $DAUTH_USERNAME --session $bw_session)
   export DAUTH_MFA=$(bw get totp $DAUTH_USERNAME --session $bw_session)
   /home/tim/workspace/tools-devutil/dist/devutil dauth
+}
+
+function fzf_clusters() {
+  declare -A clusters
+  clusters_sorted=()
+
+  supported_clusters="$(yq -r '. | keys | sort | .[]' "~/workspace/dsh-k8s-work/scripts/clusters.yaml")"
+  for cluster in $supported_clusters; do
+    clusters_sorted+=("$cluster")
+    cluster_data=$(yq ".${cluster}" "~/workspace/dsh-k8s-work/scripts/clusters.yaml" | sed "s/: /=/")
+    clusters["$cluster"]="$cluster_data"
+  done
+  echo "${clusters_sorted[@]}" | tr ' ' '\n' | fzf
 }
