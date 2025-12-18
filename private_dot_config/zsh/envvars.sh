ae8ae61ff4d6d19ff916a25fd932f29b17138fcd
tim.vaneerdewegh <tim.vaneerdewegh@klarrio.com>
2025-12-18 14:12:55 +0100

    Update .config/zsh/envvars.sh

--- private_dot_config/zsh/envvars.sh
+++ private_dot_config/zsh/envvars.sh
@@ -5,3 +5,4 @@ export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.krew/bin:$PATH
 export GOPRIVATE=github.com/klarrio-kpn-dsh/*
 export EDITOR=nvim
 export OPENAI_API_BASE=https://ai-llm.klarrio.com
+export K8S_IMAGE='rancher/k3d:latest'
