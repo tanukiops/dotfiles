1540d469eebebcd3347b531b04148ee07a5b08aa
tim.vaneerdewegh <tim.vaneerdewegh@klarrio.com>
2026-01-09 08:51:35 +0100

    Update .config/zsh/functions.sh

--- private_dot_config/zsh/empty_functions.sh
+++ private_dot_config/zsh/empty_functions.sh
@@ -1,17 +1,9 @@
 function tunnel() {
-  /home/tim/workspace/tools-devutil/dist/devutil ktunnel --name "k8s-dev-aws-lz-dsh"
+  /home/tim/.local/share/mise/installs/devutil/0.14.0/bin/devutil ktunnel --name "k8s-dev-aws-lz-dsh"
 }
 function kadmin() {
-  /home/tim/workspace/tools-devutil/dist/devutil kupdate --dev --name $(kubectl config current-context)
+  /home/tim/.local/share/mise/installs/devutil/0.14.0/bin/devutil kupdate --dev --name $(kubectl config current-context)
 }
 function kconnect() {
   kubie ctx $(yq -r '. | keys[] ' /home/tim/workspace/dsh-k8s-work/scripts/clusters.yaml | fzf)
-}
-
-function dauth() {
-  bw_session=$(bw unlock --raw)
-  export DAUTH_USERNAME="vane528"
-  export DAUTH_PASSWORD=$(bw get password $DAUTH_USERNAME --session $bw_session)
-  export DAUTH_MFA=$(bw get totp $DAUTH_USERNAME --session $bw_session)
-  /home/tim/workspace/tools-devutil/dist/devutil dauth
 }
