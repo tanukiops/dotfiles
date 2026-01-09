0e70ece88685a00a65fd9a8db8b6f9dc7be6ef87
tim.vaneerdewegh <tim.vaneerdewegh@klarrio.com>
2026-01-09 10:25:32 +0100

    Update .config/zsh/functions.sh

--- private_dot_config/zsh/empty_functions.sh
+++ private_dot_config/zsh/empty_functions.sh
@@ -1,8 +1,8 @@
 function tunnel() {
-  /home/tim/.local/share/mise/installs/devutil/0.14.0/bin/devutil tunnel --name "k8s-dev-aws-lz-dsh"
+  /home/tim/.local/share/mise/installs/devutil/0.14.1/bin/devutil tunnel --name "k8s-dev-aws-lz-dsh"
 }
 function kadmin() {
-  /home/tim/.local/share/mise/installs/devutil/0.14.0/bin/devutil kupdate --dev --name $(kubectl config current-context)
+  /home/tim/.local/share/mise/installs/devutil/0.14.1/bin/devutil kupdate --dev --name $(kubectl config current-context)
 }
 function kconnect() {
   kubie ctx $(yq -r '. | keys[] ' /home/tim/workspace/dsh-k8s-work/scripts/clusters.yaml | fzf)
