4b503082f06fa1d6823e4490efb86a989b76b4de
tim.vaneerdewegh <tim.vaneerdewegh@klarrio.com>
2026-08-07 15:01:00 +0200

    Update .config/zsh/functions.sh

--- private_dot_config/zsh/empty_functions.sh
+++ private_dot_config/zsh/empty_functions.sh
@@ -7,3 +7,18 @@ function kadmin() {
 function kconnect() {
   kubie ctx $(yq -r '. | keys[] ' /home/tim/workspace/dsh-k8s-work/scripts/clusters.yaml | fzf)
 }
+
+function platform-api() {
+
+  NS=${NS:-dsh}
+  SVC=${SVC:-platform-api-rest}
+  PORTS=${PORTS:-9000:9000}
+
+  trap 'kill 0' EXIT INT TERM # take the kubectl child down with us
+
+  while true; do
+    kubectl -n "$NS" port-forward "svc/$SVC" $PORTS
+    echo "$(date +%T) port-forward exited ($?), restarting in 1s" >&2
+    sleep 1
+  done
+}
